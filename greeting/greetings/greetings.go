d1a102493db80b0a390d9467090dcb9b914e7321
Muriel Gonzalez <muriel.gonzalez@onxmaps.com>
2022-02-27 11:26:37 -0700

    logs

--- greeting/greetings/greetings.go
+++ greeting/greetings/greetings.go
@@ -1,7 +1,13 @@
 package greetings
 
-import "fmt"
+import (
+    "fmt"
+    "errors"
+)
 
-func Hello(name string) string {
-    return fmt.Sprintf("Hi, %v. Welcome!", name)
+func Hello(name string) (string, error) {
+    if name == "" {
+        return "", errors.New("empty name")
+    }
+    return fmt.Sprintf("Hi, %v. Welcome!", name), nil
 }
--- greeting/hello/hello.go
+++ greeting/hello/hello.go
@@ -2,11 +2,22 @@ package main
 
 import (
     "fmt"
+    "log"
+
     "gonzo/greetings"
 )
 
-func main(){
-    message := greetings.Hello("Muriel")
+func main() {
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+    message, err := greetings.Hello("")
+    // message, err := greetings.Hello("Muriel")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+    
     fmt.Println(message)
 }
 
